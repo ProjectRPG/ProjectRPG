7cdb482ee074cf08c4d5794832344d603f4c8b5d
Russell <Russell.J.Baker@gmail.com>
2014-04-29 21:42:46 -0400

    added forgotten files. forgot I moved them

---
+++ src/Assets/CharacterCreation/CharacterInfoGUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CharacterCreation
+{
+    public class CharacterInfoGUI : MonoBehaviour
+    {
+	static internal string RaceName = string.Empty;
+
+        static internal string RaceDescription = string.Empty;
+
+        static internal string RaceStats = string.Empty;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnGUI()
+        {
+            var infoBox = new Rect();
+            infoBox.x = Screen.width - 500;
+            infoBox.y = 20;
+            infoBox.width = 400;
+            infoBox.height = 100;
+
+            GUI.Box(infoBox, RaceName);
+        }
+    }
+}
