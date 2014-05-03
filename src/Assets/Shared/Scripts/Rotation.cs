6828304a32e6c3c0ead0911048b42f55681b2f5d
Russell <Russell.J.Baker@gmail.com>
2014-05-03 08:10:25 -0400

    updated rotation and fixed the gitignore

--- src/Assets/Shared/Scripts/Rotation.cs
+++ src/Assets/Shared/Scripts/Rotation.cs
@@ -4,8 +4,6 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class Rotation : MonoBehaviour
 {
-    private Vector3? prevMousePos = null;
-
     private bool canMove = false;
 
     private int rotationCounter = 0;
@@ -18,10 +16,8 @@ public class Rotation : MonoBehaviour
 
     public int RotationIncrement = 1;
 
-    // Use this for initialization
     public void Start()
     {
-        Debug.Log(AccelerationFactor);
     }
 
     public void Update()
@@ -29,8 +25,8 @@ public class Rotation : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             canMove = true;
-            prevMousePos = Input.mousePosition;
         }
+
         if (Input.GetMouseButtonUp(0))
         {
             canMove = false;
@@ -39,20 +35,21 @@ public class Rotation : MonoBehaviour
 
     public void FixedUpdate()
     {
-        int currMousePos = (int)Input.mousePosition.x;
+        int currentMousePosition = (int)Input.mousePosition.x;
+
         if (canMove)
         {
-            if (currMousePos < screenCenter - CenterVoid)
+            if (currentMousePosition < screenCenter - CenterVoid)
             {
-                rotationCounter -= (int)((screenCenter - CenterVoid - currMousePos) * AccelerationFactor) + RotationIncrement;
+                rotationCounter -= (int)((screenCenter - CenterVoid - currentMousePosition) * AccelerationFactor) + RotationIncrement;
                 Vector3 rotate = new Vector3(0, rotationCounter, 0);
                 Quaternion rotationDelta = Quaternion.Euler(rotate);
                 rigidbody.MoveRotation(rotationDelta);
             }
 
-            if (currMousePos > screenCenter + CenterVoid)
+            if (currentMousePosition > screenCenter + CenterVoid)
             {
-                rotationCounter += (int)((currMousePos - screenCenter + CenterVoid) * AccelerationFactor) + RotationIncrement;
+                rotationCounter -= (int)((screenCenter + CenterVoid - currentMousePosition) * AccelerationFactor) + RotationIncrement;
                 Vector3 rotate = new Vector3(0, rotationCounter, 0);
                 Quaternion rotationDelta = Quaternion.Euler(rotate);
                 rigidbody.MoveRotation(rotationDelta);
