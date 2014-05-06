b9884d75f2c41e0891ca079868ea8930ccc99e66
Russell <Russell.J.Baker@gmail.com>
2014-05-05 20:45:16 -0400

    minor refactor

--- src/Assets/CharacterCreation/Scripts/CharacterInfoGUI.cs
+++ src/Assets/CharacterCreation/Scripts/CharacterInfoGUI.cs
@@ -11,14 +11,36 @@ namespace CharacterCreation
 
         static internal string RaceStats = string.Empty;
 
+        private float previousButtonLeft;
+
+        private float nextButtonLeft;
+
+        private float previousButtonTop;
+
+        private float nextButtonTop;
+
+        public float PreviousButtonTopPercent = 25;
+
+        public float PreviousButtonLeftPercent = 25;
+
+        public float NextButtonTopPercent = 25;
+
+        public float NextButtonLeftPercent = 25;
+
+        public float PreviousButtonWidth;
+
+        public float PreviousButtonHeight;
+
+        public float NextButtonWidth;
+
+        public float NextButtonHeight;
+
         void Start()
         {
-
-        }
-
-        void Update()
-        {
-
+            previousButtonTop = Screen.height * (PreviousButtonTopPercent / 100);
+            nextButtonTop = Screen.height * (NextButtonTopPercent / 100);
+            nextButtonLeft = Screen.height * (NextButtonLeftPercent / 100);
+            previousButtonLeft = Screen.height * (PreviousButtonLeftPercent / 100);
         }
 
         public void OnGUI()
@@ -33,13 +55,13 @@ namespace CharacterCreation
 
             GUI.Box(infoBox, RaceName);
 
-            if (GUI.Button(new Rect(Screen.width - 150, Screen.height - 100, 100, 50), "Next"))
+            if (GUI.Button(new Rect(Screen.width - nextButtonLeft - NextButtonWidth, Screen.height - nextButtonTop, NextButtonWidth, NextButtonHeight), "Next"))
             {
                 var thing2 = new Vector3(0f, 90f, 0f);
                 Camera.main.transform.Rotate(thing2);
             };
 
-            if (GUI.Button(new Rect(150, Screen.height - 100, 100, 50), "Previous"))
+            if (GUI.Button(new Rect(previousButtonLeft, Screen.height - previousButtonTop, PreviousButtonWidth, PreviousButtonHeight), "Previous"))
             {
                 var thing2 = new Vector3(0f, -90f, 0f);
                 Camera.main.transform.Rotate(thing2);
