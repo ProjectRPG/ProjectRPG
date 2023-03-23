856f3b546222aa0bf47910cbe2c658ee6609f853
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-03-22 21:15:48 -0400

    initial commit

---
+++ src/main/java/rpg/project/lib/ProjectRPG.java
@@ -0,0 +1,11 @@
+package rpg.project.lib;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ProjectRPG.MODID)
+public class ProjectRPG
+{
+    public static final String MODID = "projectrpg";
+    
+    public ProjectRPG() {}
+}
