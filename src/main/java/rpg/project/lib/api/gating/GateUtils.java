518c13f026df05df6f1eadb8257385c55e215845
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-04-29 18:11:14 -0400

    add codec registration to gatesystem registration

--- src/main/java/rpg/project/lib/api/gating/GateUtils.java
+++ src/main/java/rpg/project/lib/api/gating/GateUtils.java
@@ -1,20 +1,27 @@
 package rpg.project.lib.api.gating;
 
+import net.minecraft.resources.ResourceLocation;
+import rpg.project.lib.api.data.SubSystemConfigType;
 import rpg.project.lib.internal.registry.GateRegistry;
 import rpg.project.lib.internal.registry.GateRegistry.Type;
+import rpg.project.lib.internal.registry.SubSystemCodecRegistry;
 
 public class GateUtils {
 	//TODO JavaDocs
-	public static void registerEventGate(GateSystem system) {
+	public static void registerEventGate(ResourceLocation id, SubSystemConfigType config, GateSystem system) {
+		SubSystemCodecRegistry.registerSubSystem(id, config);
 		GateRegistry.register(system, Type.EVENT);
 	}
-	public static void registerProgressGate(GateSystem system) {
+	public static void registerProgressGate(ResourceLocation id, SubSystemConfigType config, GateSystem system) {
+		SubSystemCodecRegistry.registerSubSystem(id, config);
 		GateRegistry.register(system, Type.PROGRESS);
 	}
-	public static void registerFeatureGate(GateSystem system) {
+	public static void registerFeatureGate(ResourceLocation id, SubSystemConfigType config, GateSystem system) {
+		SubSystemCodecRegistry.registerSubSystem(id, config);
 		GateRegistry.register(system, Type.FEATURE);
 	}
-	public static void registerAbilityGate(GateSystem system) {
+	public static void registerAbilityGate(ResourceLocation id, SubSystemConfigType config, GateSystem system) {
+		SubSystemCodecRegistry.registerSubSystem(id, config);
 		GateRegistry.register(system, Type.ABILITY);
 	}
 }
