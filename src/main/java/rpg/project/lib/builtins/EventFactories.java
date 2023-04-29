35f917ed519607596d1d3a615c75171063e46ed1
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-04-29 07:10:21 -0400

    add place event

--- src/main/java/rpg/project/lib/builtins/EventFactories.java
+++ src/main/java/rpg/project/lib/builtins/EventFactories.java
@@ -4,27 +4,41 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.level.BlockEvent.BreakEvent;
 import net.minecraftforge.event.level.BlockEvent.EntityPlaceEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.EventPriority;
 import rpg.project.lib.api.data.ObjectType;
 import rpg.project.lib.api.events.EventContext;
 import rpg.project.lib.api.events.EventListenerSpecification;
+import rpg.project.lib.internal.util.Reference;
 import rpg.project.lib.internal.util.RegistryUtil;
 
-/**Contains default factories for translating {@link net.minecraftforge.eventbus.api.Event Event}s 
- * to {@link EventContext}s for use by declarations in {@link rpg.project.lib.internal.registry.EventRegistry EventRegistry}
+/**Contains default factories for translating 
+ * {@link net.minecraftforge.eventbus.api.Event Event}s 
+ * to {@link EventContext}s for use by declarations in 
+ * {@link rpg.project.lib.internal.registry.EventRegistry EventRegistry}
  * for relevant ecosystem events.
  */
-public class EventFactories {
+public class EventFactories {	
+	public static final EventListenerSpecification<BreakEvent> BLOCK_BREAK = new EventListenerSpecification<>(
+			Reference.resource("break_block"),
+			EventPriority.LOWEST,
+			BreakEvent.class,
+			event -> true,
+			event -> EventContext.build(ObjectType.BLOCK, RegistryUtil.getId(event.getState()), event.getPlayer())
+				.withPos(event.getPos()).build(),
+			EventFactories::fullCancel,
+			(event, vars) -> {});
+	
+	public static final EventListenerSpecification<EntityPlaceEvent> BLOCK_PLACE = new EventListenerSpecification<>(
+			Reference.resource("place_block"), 
+			EventPriority.LOWEST, 
+			EntityPlaceEvent.class,
+			event -> event.getEntity() instanceof Player,
+			event -> EventContext.build(ObjectType.BLOCK, RegistryUtil.getId(event.getState()), (Player)event.getEntity())
+				.withPos(event.getPos()).build(),
+			EventFactories::fullCancel,
+			(event, vars) -> {});
+	
 	public static void fullCancel(Event event, EventListenerSpecification.CancellationType cancelType) {
 		event.setCanceled(true);
-	}
-	
-	public static EventContext breakBlock(BreakEvent event) {
-		return EventContext.build(ObjectType.BLOCK, RegistryUtil.getId(event.getState()), event.getPlayer())
-				.withPos(event.getPos()).build();
-	}
-	
-	public static EventContext placeBlock(EntityPlaceEvent event) {
-		return EventContext.build(ObjectType.BLOCK, RegistryUtil.getId(event.getState()), (Player) event.getEntity())
-				.withPos(event.getPos()).build();
 	}
 }
