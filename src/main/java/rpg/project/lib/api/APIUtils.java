67cbaee707e02b979f2aa8c33572f523c473026e
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-05-12 06:50:05 -0400

    expose dispatch codec safely.

--- src/main/java/rpg/project/lib/api/APIUtils.java
+++ src/main/java/rpg/project/lib/api/APIUtils.java
@@ -1,8 +1,12 @@
 package rpg.project.lib.api;
 
+import com.mojang.serialization.Codec;
+
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import rpg.project.lib.api.data.SubSystemConfigType;
 import rpg.project.lib.api.events.EventListenerSpecification;
+import rpg.project.lib.internal.registry.SubSystemCodecRegistry;
 import rpg.project.lib.internal.util.Reference;
 
 public class APIUtils {
@@ -10,4 +14,8 @@ public class APIUtils {
 	 * to register custom events.  Custom events can be referenced by other addons' configurations to apply their
 	 * system implementation behavior to your event specification.*/
 	public static final ResourceKey<Registry<EventListenerSpecification<?>>> GAMEPLAY_EVENTS = ResourceKey.createRegistryKey(Reference.resource("gameplay_events"));
+	
+	public static Codec<SubSystemConfigType> getDispatchCodec() {
+		return SubSystemCodecRegistry.CODEC;
+	}
 }
